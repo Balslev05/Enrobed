735142dfad71ffbb6b0627d45346e44ff21255ff
Zsef <114775729+Balslev05@users.noreply.github.com>
2023-09-10 01:27:40 +0200

    Updatet Coding_Rules & added Testing Scenes

--- Game/Assets/B_ReadMe/Code_Rules.cs
+++ Game/Assets/B_ReadMe/Code_Rules.cs
@@ -1,3 +1,4 @@
+//----------------------------- ODIN INSPECTOR -----------------------------//
 // Vi bruger noget der hedder Odin inspector som er bare en bedre inspector --
 // så vi skal huske at skrive i toppen af alle scripts: using Sirenix.OdinInspector;
 // Det gør så også at der hvor der normal står monobehavior der skal i ændre det til SerializedMonoBehaviour HUSK DET MEGET VIGTIG ELLERS FUNGERE SCRIPTSNE IKKE 
@@ -5,17 +6,21 @@
 //  hvis i er i tvivl om hvordan man bruger det så - tools/odin/inspector/attribute  window så kan i se alt documentationen
 
 
+//----------------------------- VARIABLER & FUNCTIONER -----------------------------//
 // Private Varibels is set with a _ fx. speed = wrong ||  _speed = right
-
 // public  Varibels is set with without a _ fx speed = right ||  _speed = Wrong
-
-
-// Varibels firstletter always  starts with a small letter  
-
+// Varibels firstletter always  starts with a small letter
 // Functions firstletter always  starts with a Big letter  
 
-
-//Når i pusher i github så HUSK at Titlen af hvad i ændrede/addede
-
+//----------------------------- GITHUB -----------------------------//
+// husk at hente den nyeste Version af projektet ned vær gang du begynder at lave på projectet 
+// Husk at Push/uploaded vær gang i har lavet noget der dur og i færdig for dagen 
+// Når i pusher i github så HUSK at Titlen af hvad i ændrede/addede
 // Hvis i har nogle spørgsmål så skriv til mig 
+// Hvis i har lavet noget færdigt men der sker en fejl når i publisher så skriv det i stedet for at løse det på stedet med mindre i ved hvad grunden er :D
 
+
+//----------------------------- Ligegyldig Info -----------------------------//
+// Hvis i bryder nogle af reglerne så sker der selvfølelig ikke noget, projektet her for hyggenskyld & det er en god måde på vi alle kan øve os på.
+// hvis der kommer rod i projektet pga nogle regler der er blevet brudt så skal vi nok løse det på en eller anden måde.
+// Slap af og hyg dig med projektet når du har lyst, der er selfølelig ingen deadlines der skal holdes.
