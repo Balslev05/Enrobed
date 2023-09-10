e80f1d3d51e8009e0ee1426a3a103bf89a630a98
Zsef <114775729+Balslev05@users.noreply.github.com>
2023-09-10 13:38:00 +0200

    Fixing small things

---
+++ Game/Assets/A_Code/CharacterSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+using DG.Tweening;
+using TMPro;
+public class CharacterSystem : SerializedMonoBehaviour
+{
+    public Npc_Template characterPersonality;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Game/Assets/A_Code/PlayerDialog.cs
+++ Game/Assets/A_Code/PlayerDialog.cs
@@ -1,26 +1,52 @@
+using System;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using DG.Tweening;
 using TMPro;
 public class PlayerDialog : SerializedMonoBehaviour
 {
+    [ShowInInspector] 
+    [TabGroup("PrivateVariable")]
     private Npc_Template _talkingTo;
 
-    public Transform closesttNpc;
+    [ShowInInspector] 
+    [TabGroup("PrivateVariable")]
+    public GameObject[] npcs;
+
+    [ShowInInspector] 
+    [TabGroup("PrivateVariable")]
+    private float _distance;
     
-    void Start()
+    [ShowInInspector] 
+    [TabGroup("PrivateVariable")]
+    private float _nearestdistance;
+    
+    [ShowInInspector] 
+    [TabGroup("PrivateVariable")]
+    private GameObject _closestNpc;
+    
+    // Update is called once per frame
+     void Start()
     {
-        
+        npcs = GameObject.FindGameObjectsWithTag("Npc");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        closesttNpc = GameObject.FindGameObjectWithTag("Npc").transform;
-
-        if (Vector2.Distance(transform.position, closesttNpc.transform.position) < 1)
+        for (int i = 0; i < npcs.Length; i++)
         {
-            Debug.Log("CloseTo a talking npc");
+            _distance = Vector2.Distance(this.transform.position, npcs[i].transform.position);
+            
+            if (_distance < _nearestdistance)
+            {
+                _closestNpc = npcs[i];
+                _nearestdistance = _distance;
+            }
+        }
+        
+        if (Vector2.Distance(transform.position, _closestNpc.transform.position) <= 2 && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log(_closestNpc.GetComponent<CharacterSystem>().characterPersonality.name);
         }
     }
 }
